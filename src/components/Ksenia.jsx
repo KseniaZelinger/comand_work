3a91fc238330839b057f45ae79fe87302823047f
Ксения Зелингер <Zelingerksenia@gmail.com>
2022-11-14 16:01:51 +0300

    таблица из ant

--- src/components/Ksenia.jsx
+++ src/components/Ksenia.jsx
@@ -1,19 +1,26 @@
 import { Button } from 'antd';
 import React from 'react';
+import { Table } from 'antd';
+import TableKsenia from './ksenia-components/table';
 
 function Ksenia() {
 
     return (
-        <div>
+        <><div>
             <Button type="primary">Primary Button</Button>
             <Button>Default Button</Button>
             <Button type="dashed">Dashed Button</Button>
             <br />
             <Button type="text">Text Button</Button>
             <Button type="link">Link Button</Button>
-
         </div>
+
+            <div style={{ width: '80%', margin: '0 auto' }}>
+                <TableKsenia />
+            </div></>
+
     )
 }
 
 export default Ksenia;
+
---
+++ src/components/ksenia-components/table.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Table } from 'antd';
+
+
+function TableKsenia() {
+    const data = [
+        {
+            name: "J7",
+            product: "Сок",
+            price: "115p."
+
+        },
+        {
+            name: "Сады Придонья",
+            product: "Нектар",
+            price: "85p."
+
+        },
+        {
+            name: "Добрый",
+            product: "Нектар",
+            price: "99p."
+
+        },
+        {
+            name: "Rich",
+            product: "Сок",
+            price: "148p."
+
+        },
+        {
+            name: "Я",
+            product: "Сок",
+            price: "136p."
+
+        },
+        {
+            name: "Фруктовый сад",
+            product: "Нектар",
+            price: "89p."
+
+        }
+    ]
+
+    const colums = [
+        {
+            title: "Название",
+            dataIndex: "name",
+            key: "name"
+        },
+        {
+            title: "Тип продукта",
+            dataIndex: "product",
+            key: "product"
+        },
+        {
+            title: "Цена",
+            dataIndex: "price",
+            key: "price"
+        }
+    ]
+    return (
+        <>
+            <Table dataSource={data} colums={colums} width={500} />
+        </>
+    )
+}
+
+export default TableKsenia;
